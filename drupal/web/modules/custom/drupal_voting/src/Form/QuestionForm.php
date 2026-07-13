d069b5e0c1e03ea9561e93faaf9111e519901207
Eric Vinicius <eu@ericvinicius.com.br>
2026-07-12 23:15:28 -0300

    Making the identifier behave like a machine name field for unicity.

--- drupal/web/modules/custom/drupal_voting/src/Form/QuestionForm.php
+++ drupal/web/modules/custom/drupal_voting/src/Form/QuestionForm.php
@@ -12,36 +12,88 @@ use Drupal\Core\Form\FormStateInterface;
  */
 final class QuestionForm extends ContentEntityForm {
 
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+    $form = parent::buildForm($form, $form_state);
+
+    if (isset($form['identifier']['widget'][0]['value'])) {
+      $identifier = &$form['identifier']['widget'][0]['value'];
+      $identifier['#type'] = 'machine_name';
+
+      $identifier['#machine_name'] = [
+        'exists' => [
+          $this,
+          'identifierExists',
+        ],
+        'source' => [
+          'label',
+          'widget',
+          0,
+          'value',
+        ],
+        'replace_pattern' => '[^a-z0-9-]+',
+        'replace' => '-',
+      ];
+
+    }
+
+    if ($this->entity->isNew()) {
+      $form['actions']['submit']['#value'] = $this->t('Save and Configure Answer Options');
+    }
+
+    return $form;
+  }
+
   /**
    * {@inheritdoc}
    */
   public function save(array $form, FormStateInterface $form_state): int {
+
+    $is_new = $this->entity->isNew();
+
     $result = parent::save($form, $form_state);
 
-    $message_args = ['%label' => $this->entity->toLink()->toString()];
-    $logger_args = [
-      '%label' => $this->entity->label(),
-      'link' => $this->entity->toLink($this->t('View'))->toString(),
-    ];
+    if ($is_new) {
+      $this->messenger()->addStatus(
+        $this->t('Question created successfully.')
+      );
 
-    switch ($result) {
-      case SAVED_NEW:
-        $this->messenger()->addStatus($this->t('New question %label has been created.', $message_args));
-        $this->logger('drupal_voting')->notice('New question %label has been created.', $logger_args);
-        break;
+      $form_state->setRedirect(
+        'entity.drupal_voting_question.options',
+        [
+          'drupal_voting_question' => $this->entity->id(),
+        ]
+      );
+    }
+    else {
+      $this->messenger()->addStatus(
+        $this->t('Question updated successfully.')
+      );
 
-      case SAVED_UPDATED:
-        $this->messenger()->addStatus($this->t('The question %label has been updated.', $message_args));
-        $this->logger('drupal_voting')->notice('The question %label has been updated.', $logger_args);
-        break;
-
-      default:
-        throw new \LogicException('Could not save the entity.');
+      $form_state->setRedirect('entity.drupal_voting_question.collection');
     }
 
-    $form_state->setRedirectUrl($this->entity->toUrl());
-
     return $result;
+  }
+
+  /**
+   * Checks whether a Question identifier already exists.
+   */
+  public function identifierExists(string $identifier): bool {
+    $query = $this->entityTypeManager
+      ->getStorage('drupal_voting_question')
+      ->getQuery()
+      ->accessCheck(FALSE)
+      ->condition('identifier', $identifier);
+
+    if (!$this->entity->isNew()) {
+      $query->condition('id', $this->entity->id(), '<>');
+    }
+
+    return (bool) $query->range(0, 1)->count()->execute();
   }
 
 }
